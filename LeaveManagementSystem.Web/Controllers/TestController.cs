3c62a726e812bbbeaeb544f96325b1158fa16c6e
Anusha Kotagiri <anusha.kotagiri.anu@gmail.com>
2024-08-11 13:57:46 +0530

    Add project files.

---
+++ LeaveManagementSystem.Web/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using LeaveManagementSystem.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LeaveManagementSystem.Web.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            var data = new TestViewModel{ Name = "student", 
+            DateOfBirth = new DateTime(1997,04,26)};
+            return View(data);
+        }
+    }
+}
